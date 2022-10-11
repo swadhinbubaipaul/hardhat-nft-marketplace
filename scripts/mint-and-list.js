08e14e21731d210955b4a47d6782a1b1588bdabd
swadhinbubaipaul <swadhinpaul159@gmail.com>
2022-10-11 12:17:06 +0530

    Added an extra utils

--- scripts/mint-and-list.js
+++ scripts/mint-and-list.js
@@ -1,4 +1,5 @@
-const { ethers } = require("hardhat");
+const { ethers, network } = require("hardhat");
+const { moveBlocks } = require("../utils/move-blocks");
 
 const PRICE = ethers.utils.parseEther("0.1");
 async function mintAndList() {
@@ -24,6 +25,10 @@ async function mintAndList() {
     console.log("NFT Listed!");
   } else {
     console.log("Failed to mint NFT!!!");
+  }
+
+  if (network.config.chainId == "31337") {
+    await moveBlocks(2, (sleepAmount = 1000));
   }
 }
 
