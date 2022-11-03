54dac6248b9c8c02643ad0da50b237f1fc819649
swadhinbubaipaul <swadhinpaul159@gmail.com>
2022-11-03 19:57:59 +0530

    added script for minting NFT

---
+++ scripts/mint.js
@@ -0,0 +1,22 @@
+const { ethers, network } = require("hardhat");
+const { moveBlocks } = require("../utils/move-blocks");
+
+async function mint() {
+  const basicNft = await ethers.getContract("BasicNft");
+  const mintTx = await basicNft.mintNft();
+  console.log("Minting NFT...");
+  const mintTxReceipt = await mintTx.wait(1);
+  const tokenId = mintTxReceipt.events[0].args.tokenId;
+  console.log(`Got TokenId: ${tokenId}`);
+  console.log(`NFT Address: ${basicNft.address}`);
+  if (network.config.chainId == "31337") {
+    await moveBlocks(2, (sleepAmount = 1000));
+  }
+}
+
+mint()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
